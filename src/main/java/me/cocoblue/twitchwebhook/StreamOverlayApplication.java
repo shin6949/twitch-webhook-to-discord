ceda00b9539f2fb836371e486ff114bf2f14747c
cocoblue <cocoblue@kakao.com>
2021-09-17 12:19:27 +0900

    외부 WAS에서 구동할 수 있도록 수정

--- src/main/java/me/cocoblue/twitchwebhook/StreamOverlayApplication.java
+++ src/main/java/me/cocoblue/twitchwebhook/StreamOverlayApplication.java
@@ -2,11 +2,18 @@ package me.cocoblue.twitchwebhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class StreamOverlayApplication {
+public class StreamOverlayApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StreamOverlayApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(StreamOverlayApplication.class, args);
     }
