79f2214f44671132a22505e01abe4c2e0958ae9a
shin6949 <cocoblue@kakao.com>
2022-08-29 21:14:52 +0900

    - View Mapping Test

---
+++ src/main/java/me/cocoblue/twitchwebhook/domain/SubscriptionGroupViewRepository.java
@@ -0,0 +1,7 @@
+package me.cocoblue.twitchwebhook.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionGroupViewRepository extends JpaRepository<SubscriptionGroupViewEntity, SubscriptionGroupViewId> {
+
+}
