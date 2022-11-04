0a7f68de6999545f2b009690b3b1b32f5469ee75
shin6949 <cocoblue@kakao.com>
2022-11-04 10:21:17 +0900

    2.9.0 SQL 구문 통합

--- assets/migrate_to_2.9.0-test4.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE youtube_channel_info DROP upload_playlist_id;
-ALTER TABLE youtube_channel_info DROP last_checked_time;
-ALTER TABLE youtube_channel_info ADD meno VARCHAR(1000) default null;
